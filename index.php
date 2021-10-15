1f655671936fc79ec10bb1ead785c40acd7780eb
Diego_Beltran <dbeltranpereira@gmail.com>
2021-10-14 22:53:42 -0500

    creacion de sitio y estructura de carpetas

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Biblioteca</title>
+
+  <link rel="stylesheet" href="./css/bootstrap.min.css" />
+</head>
+
+<body>
+  <nav class="navbar navbar-expand navbar-light bg-light">
+    <ul class="nav navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Nav 1 <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Nav 2</a>
+      </li>
+    </ul>
+  </nav>
+</body>
+
+</html>
